a3fc8886d8bbca7d72595a815bc4e13e404ae44d
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-13 10:23:30 +0530

    Add files via upload
    This app is used to convert excel to dataset and json

---
+++ ExcelToDatasetAndJson/ExcelToDatasetAndJson/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ExcelToDatasetAndJson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtExcelPath = new System.Windows.Forms.TextBox();
+            this.btnBrowseExcel = new System.Windows.Forms.Button();
+            this.btnBrowseConfig = new System.Windows.Forms.Button();
+            this.txtConfigPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDownloadSampleConfig = new System.Windows.Forms.Button();
+            this.txtSheetName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Excel Path";
+            // 
+            // txtExcelPath
+            // 
+            this.txtExcelPath.Location = new System.Drawing.Point(121, 47);
+            this.txtExcelPath.Name = "txtExcelPath";
+            this.txtExcelPath.Size = new System.Drawing.Size(477, 20);
+            this.txtExcelPath.TabIndex = 1;
+            this.txtExcelPath.TextChanged += new System.EventHandler(this.txtExcelPath_TextChanged);
+            // 
+            // btnBrowseExcel
+            // 
+            this.btnBrowseExcel.Location = new System.Drawing.Point(604, 46);
+            this.btnBrowseExcel.Name = "btnBrowseExcel";
+            this.btnBrowseExcel.Size = new System.Drawing.Size(116, 22);
+            this.btnBrowseExcel.TabIndex = 2;
+            this.btnBrowseExcel.Text = "Browse Excel";
+            this.btnBrowseExcel.UseVisualStyleBackColor = true;
+            this.btnBrowseExcel.Click += new System.EventHandler(this.btnBrowseExcel_Click);
+            // 
+            // btnBrowseConfig
+            // 
+            this.btnBrowseConfig.Location = new System.Drawing.Point(604, 167);
+            this.btnBrowseConfig.Name = "btnBrowseConfig";
+            this.btnBrowseConfig.Size = new System.Drawing.Size(116, 22);
+            this.btnBrowseConfig.TabIndex = 5;
+            this.btnBrowseConfig.Text = "Browse Config";
+            this.btnBrowseConfig.UseVisualStyleBackColor = true;
+            this.btnBrowseConfig.Click += new System.EventHandler(this.btnBrowseConfig_Click);
+            // 
+            // txtConfigPath
+            // 
+            this.txtConfigPath.Location = new System.Drawing.Point(121, 168);
+            this.txtConfigPath.Name = "txtConfigPath";
+            this.txtConfigPath.Size = new System.Drawing.Size(477, 20);
+            this.txtConfigPath.TabIndex = 4;
+            this.txtConfigPath.TextChanged += new System.EventHandler(this.txtConfigPath_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Config Path";
+            // 
+            // btnDownloadSampleConfig
+            // 
+            this.btnDownloadSampleConfig.Location = new System.Drawing.Point(604, 203);
+            this.btnDownloadSampleConfig.Name = "btnDownloadSampleConfig";
+            this.btnDownloadSampleConfig.Size = new System.Drawing.Size(116, 22);
+            this.btnDownloadSampleConfig.TabIndex = 6;
+            this.btnDownloadSampleConfig.Text = "Download Sample Config";
+            this.btnDownloadSampleConfig.UseVisualStyleBackColor = true;
+            this.btnDownloadSampleConfig.Click += new System.EventHandler(this.btnDownloadSampleConfig_Click);
+            // 
+            // txtSheetName
+            // 
+            this.txtSheetName.Location = new System.Drawing.Point(121, 88);
+            this.txtSheetName.Name = "txtSheetName";
+            this.txtSheetName.Size = new System.Drawing.Size(477, 20);
+            this.txtSheetName.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter sheet name";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(313, 237);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 9;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(121, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(483, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Note :- If sheet name is \"Sheet1\" no need to enter otherwise please enter the she" +
+    "et\r\nname. Case sensitive";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(531, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Made by CPJ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtSheetName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDownloadSampleConfig);
+            this.Controls.Add(this.btnBrowseConfig);
+            this.Controls.Add(this.txtConfigPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseExcel);
+            this.Controls.Add(this.txtExcelPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtExcelPath;
+        private System.Windows.Forms.Button btnBrowseExcel;
+        private System.Windows.Forms.Button btnBrowseConfig;
+        private System.Windows.Forms.TextBox txtConfigPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDownloadSampleConfig;
+        private System.Windows.Forms.TextBox txtSheetName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ExcelToDatasetAndJson/ExcelToDatasetAndJson/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ExcelToDatasetAndJson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Title = "Select Excel File";
+
+                //openFileDialog1.InitialDirectory = @"C:\";//--"C:\\";
+                //openFileDialog1.Filter = "All files (*.*)|*.*|Text File (*.txt)|*.txt";
+
+                openFileDialog1.Filter = "Excel Files|*.xlsx;*.xls";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.ShowDialog();
+                if (openFileDialog1.FileName != "")
+                {
+                    txtExcelPath.Text = openFileDialog1.FileName;
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ShowMessage(string stringMsg, string stringTitle, MessageBoxButtons objMsgButtons, MessageBoxIcon objMsgIcon)
+        {
+            DialogResult result = MessageBox.Show(stringMsg, stringTitle, objMsgButtons, objMsgIcon);
+            if (result == DialogResult.Yes)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void txtExcelPath_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtExcelPath.TextLength > 0)
+                {
+                    if (!ValidatePath(txtExcelPath.Text))
+                    {
+                        ShowMessage("Please select valid file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtExcelPath.Text = string.Empty;
+                    }
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidatePath(string stringPath)
+        {
+            try
+            {
+                if (File.Exists(stringPath))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return false;
+        }
+
+        private DataSet ConvertExcelToDataSet(string stringPath, string stringSheetName)
+        {
+            DataSet objDataSetReturn = new DataSet();
+            stringSheetName = stringSheetName + "$";
+            try
+            {
+                string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + stringPath + ";Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1;\"";
+                // The "HDR=YES" option indicates that the first row of the worksheet contains column headers.
+                // The "IMEX=1" option tells the provider to always read intermixed data types as text.
+
+                OleDbConnection connection = new OleDbConnection(connectionString);
+                connection.Open();
+                
+                DataTable schemaTable = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                bool found = false;
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    if (row["TABLE_NAME"].ToString() == stringSheetName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [" + stringSheetName + "]", connection);
+                    adapter.Fill(objDataSetReturn);
+
+                    connection.Close();
+                    return objDataSetReturn;
+                }
+                else
+                {
+                    ShowMessage("GIven sheet name " + stringSheetName + " , not exist in the selected excel", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        private void btnBrowseConfig_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Title = "Select XML File";
+
+                openFileDialog1.Filter = "XML Files|*.xml;";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.ShowDialog();
+                if (openFileDialog1.FileName != "")
+                {
+                    txtConfigPath.Text = openFileDialog1.FileName;
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDownloadSampleConfig_Click(object sender, EventArgs e)
+        {
+            string stringSaveFile = "";
+            byte[] bytes = null;
+            try
+            {
+                SaveFileDialog objSaveFileDialog = new SaveFileDialog();
+                objSaveFileDialog.Filter = "XML File|*.xml";
+                objSaveFileDialog.FileName = "Sample Config";
+                DialogResult result = objSaveFileDialog.ShowDialog();
+                stringSaveFile = objSaveFileDialog.FileName;
+                if (stringSaveFile.Length > 0)
+                {
+                    if (result != DialogResult.Cancel)
+                    {
+                        bytes = File.ReadAllBytes("Data/Config.xml");
+                        File.WriteAllBytes(stringSaveFile, bytes);
+                        ShowMessage("Sample config saved successfully", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtConfigPath_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtConfigPath.TextLength > 0)
+                {
+                    if (!ValidatePath(txtConfigPath.Text))
+                    {
+                        ShowMessage("Please select valid file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtConfigPath.Text = string.Empty;
+                    }
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            DataSet objDataSetInput = new DataSet();
+            DataSet objDataSetConfig = new DataSet();
+            DataTable objDataTableTemp = new DataTable();
+            DataSet objDataSetFinal = new DataSet();
+            string stringSheetName = "";
+            try
+            {
+                if (txtExcelPath.TextLength > 0 && txtConfigPath.TextLength > 0)
+                {
+                    objDataSetFinal.Tables.Add(new DataTable());
+                    stringSheetName = txtSheetName.Text.Trim().Length == 0 ? "Sheet1" : txtSheetName.Text.Trim();
+                    objDataSetInput = ConvertExcelToDataSet(txtExcelPath.Text, stringSheetName);
+
+                    if (objDataSetInput != null && objDataSetInput.Tables[0] != null && objDataSetInput.Tables[0].Rows.Count > 0)
+                    {
+                        objDataSetConfig.ReadXml(txtConfigPath.Text, XmlReadMode.Auto);
+                        objDataTableTemp = objDataSetConfig.Tables["t2"].Select("", "list_view_position asc").CopyToDataTable();
+
+                        for (int i = 0; i < objDataTableTemp.Rows.Count; i++)
+                        {
+                            if (objDataTableTemp.Rows[i]["target_data_type"].ToString().ToUpper() == "DOUBLE")
+                            {
+                                if (!objDataSetFinal.Tables[0].Columns.Contains(objDataTableTemp.Rows[i]["excel_column_name"].ToString().ToUpper()))
+                                {
+                                    objDataSetFinal.Tables[0].Columns.Add(objDataTableTemp.Rows[i]["excel_column_name"].ToString().ToUpper(), typeof(double));
+                                }
+                            }
+                            else
+                            {
+                                if (!objDataSetFinal.Tables[0].Columns.Contains(objDataTableTemp.Rows[i]["excel_column_name"].ToString().ToUpper()))
+                                {
+                                    objDataSetFinal.Tables[0].Columns.Add(objDataTableTemp.Rows[i]["excel_column_name"].ToString().ToUpper(), typeof(string));
+                                }
+                            }
+                        }
+
+                        objDataSetFinal.Tables[0].Merge(objDataSetInput.Tables[0], true, MissingSchemaAction.Ignore);
+
+                        for (int i = 0; i < objDataTableTemp.Rows.Count; i++)
+                        {
+                            string stringColumnName = objDataTableTemp.Rows[i]["excel_column_name"].ToString();
+                            string stringTargetName = objDataTableTemp.Rows[i]["target_name"].ToString();
+
+                            objDataSetFinal.Tables[0].Columns[stringColumnName].ColumnName = stringTargetName;
+                        }
+
+                        bool boolAskSaveAsXML = ShowMessage("Do you want to save as xml file", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if (boolAskSaveAsXML)
+                            SaveXmlFile(objDataSetFinal);
+
+                        bool boolAskSaveAsJson = ShowMessage("Do you want to save as json file", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if (boolAskSaveAsJson)
+                        {
+                            var settings = new JsonSerializerSettings
+                            {
+                                ContractResolver = new DefaultContractResolver
+                                {
+                                    NamingStrategy = new DefaultNamingStrategy() // Or use another naming strategy that suits your needs
+                                },
+                                Formatting = Formatting.Indented,
+                            };
+                            string stringJson = JsonConvert.SerializeObject(objDataSetFinal.Tables[0], settings);
+                            SaveJsonFile(stringJson);
+                        }
+                    }
+                    else
+                    {
+                        ShowMessage("Excel is null or empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    ShowMessage("Select excel and config xml", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveXmlFile(DataSet objDataSetInput)
+        {
+            string stringSaveFile = "";
+            try
+            {
+                SaveFileDialog objSaveFileDialog = new SaveFileDialog();
+                objSaveFileDialog.Filter = "XML File|*.xml";
+                objSaveFileDialog.FileName = "Output";
+                DialogResult result = objSaveFileDialog.ShowDialog();
+                stringSaveFile = objSaveFileDialog.FileName;
+                if (stringSaveFile.Length > 0)
+                {
+                    if (result != DialogResult.Cancel)
+                    {
+                        objDataSetInput.WriteXml(stringSaveFile, XmlWriteMode.WriteSchema);
+                        ShowMessage("Output saved successfully", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveJsonFile(string stringInput)
+        {
+            string stringSaveFile = "";
+            try
+            {
+                SaveFileDialog objSaveFileDialog = new SaveFileDialog();
+                objSaveFileDialog.Filter = "JSON File|*.json";
+                objSaveFileDialog.FileName = "Json string";
+                DialogResult result = objSaveFileDialog.ShowDialog();
+                stringSaveFile = objSaveFileDialog.FileName;
+                if (stringSaveFile.Length > 0)
+                {
+                    if (result != DialogResult.Cancel)
+                    {
+                        File.WriteAllText(stringSaveFile, stringInput);
+                        ShowMessage("Json string saved successfully", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception objException)
+            {
+                ShowMessage(objException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
